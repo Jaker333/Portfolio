6ccc9d657a487e97ecd9f3a0567aeccf8bd2f344
deromajatwit <realjaker333@gmail.com>
2026-07-15 20:57:50 -0400

    the real commit

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -29,7 +29,7 @@ const Hero = () => {
 
             {/* JAKE MIRROR */}
             <img
-                src={`https://Jaker333.github.io/Portfolio/assets/jakemirror1.gif`}
+                src={`https://jaker333.github.io/Portfolio/assets/jakemirror1.gif`}
                 alt="Jake Mirror"
                 className="absolute right-10 top-24 w-112.5 h-auto object-contain"
             />
